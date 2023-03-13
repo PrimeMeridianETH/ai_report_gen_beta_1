6680c7bed5f9e9930c1457f0732f607ee226eaf3
PrimeMeridian.eth <120077583+PrimeMeridianETH@users.noreply.github.com>
2023-03-12 23:17:18 -0700

    Update ai_analysis_report_generator.py

--- ai_analysis_report_generator.py
+++ ai_analysis_report_generator.py
@@ -17,7 +17,7 @@ if len(article_text) > 5:
     if st.button ('Generate Report'):
         response = openai.Completion.create(
             engine = "text-davinci-003",
-            prompt = "Create an analysis report from supplied material. The report should be written in a formal tone and should not repeat any information between or within sections. It should provide accurate and detailed information, with short descriptions for all data points and their implications. The report sections should include an Executive Summary, Introduction, Methodology, Results, Discussion, Conclusions, and Keywords (essential topics and subjects mentioned in the report). The target audience for the report will be consultants and contractors within the consulting firm. Use only the text information located here: " + article_text,
+            prompt = "Create an analysis report. The report should be written in a formal tone and should not repeat any information between or within sections. It should provide accurate and detailed information, with short descriptions for all data points and their implications. The report sections should include an Executive Summary, Introduction, Methodology, Results, Discussion, Conclusions, and Keywords (essential topics and subjects mentioned in the report). The target audience for the report will be consultants and contractors within the consulting firm. Use only the text information located here: " + article_text,
             max_tokens = 2000,
             presence_penalty=0.5,
             temperature = temp
